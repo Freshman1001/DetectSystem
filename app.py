c55e8a16d16328be25b096f785cdd9ff48a3fa14
Dehong Chen <98007265+Freshman1001@users.noreply.github.com>
2024-04-02 23:48:56 +0800

    初始化提交

--- app.py
+++ app.py
@@ -1,138 +1,9 @@
 # 导入Flask 模板
 from flask import Flask, render_template, request, redirect, jsonify, url_for
-# 导入要用到的数据库 第三方库
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+import db_option
+from datetime import datetime
 
 app = Flask(__name__)
-
-# 连接数据库的配置
-# MySQL所在主机名
-HOSTNAME = "127.0.0.1"
-# MySQL监听的端口号，默认3306
-PORT = 3306
-# 连接MySQL的用户名，自己设置
-USERNAME = "cora"
-# 连接MySQL的密码，自己设置
-PASSWORD = "cora1234"
-# MySQL上创建的数据库名称
-DATABASE = "detector"
-# 通过修改以下代码来操作不同的SQL比写原生SQL简单很多 --》通过ORM可以实现从底层更改使用的SQL
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
-db = SQLAlchemy(app)
-
-
-# 定义模型类user
-class User(db.Model):
-    __tablename__ = "users"
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_name = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-
-    def __repr__(self):
-        return f"<User user_id={self.user_id}, user_name={self.user_name}>"
-    # 表中的属性已经建好：user_id(key) user_name password
-    # id = db.Column(db.Integer, primary_key=True, comment="用户id")
-    # password = db.Column(db.String(20), nullable=False, comment="用户密码")
-
-
-# 定义模型类video
-class Video(db.Model):
-    __tablename__ = "videos"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    v_class = db.Column(db.String(255), nullable=True)
-    description = db.Column(db.String(20), nullable=True)
-    v_time = db.Column(db.Time, nullable=True)
-    video_path = db.Column(db.String(255), nullable=False)
-
-    def __repr__(self):
-        return f"<Video id={self.id}, v_class={self.v_class}>"
-
-
-# 查询用户
-def get_user_by_id(user_id):
-    with app.app_context():
-        return User.query.filter_by(user_id=user_id).first()
-
-
-# 添加用户
-def add_user(username, password):
-    with app.app_context():
-        new_user = User(user_name=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-
-
-# 删除用户
-def delete_user(user_id):
-    with app.app_context():
-        user = User.query.get(user_id)
-        if user:
-            db.session.delete(user)
-            db.session.commit()
-
-
-# 修改用户密码
-def update_user_password(user_id, new_password):
-    with app.app_context():
-        user = User.query.get(user_id)
-        if user:
-            user.password = new_password
-            db.session.commit()
-
-
-# 添加视频
-def add_video(v_class, description, v_time, video_path):
-    with app.app_context():
-        new_video = Video(v_class=v_class, description=description, v_time=v_time, video_path=video_path)
-        db.session.add(new_video)
-        db.session.commit()
-
-
-# 删除视频
-def delete_video(video_id):
-    with app.app_context():
-        video = Video.query.get(video_id)
-        if video:
-            db.session.delete(video)
-            db.session.commit()
-
-
-# 修改视频信息
-def update_video(video_id, new_v_class, new_description, new_v_time, new_video_path):
-    with app.app_context():
-        video = Video.query.get(video_id)
-        if video:
-            video.v_class = new_v_class
-            video.description = new_description
-            video.v_time = new_v_time
-            video.video_path = new_video_path
-            db.session.commit()
-
-
-# 根据视频ID查询视频信息
-def get_video_by_id(video_id):
-    with app.app_context():
-        return Video.query.get(video_id)
-
-
-# 根据视频分类查询视频信息
-def get_videos_by_class(v_class):
-    with app.app_context():
-        return Video.query.filter_by(v_class=v_class).all()
-
-
-# 查询所有视频信息
-def get_all_videos():
-    with app.app_context():
-        return Video.query.all()
-
-
-# 执行所有的创建表格的语句
-# 表已经建好了
-# with app.app_context():
-#     db.create_all()
 
 # # 登录的路由
 # @app.route('/login', methods=["GET", "POST"])
@@ -154,37 +25,69 @@ def admin():
 
 # 处理提交视频文件，返回提交是否成功
 
-# 查询处理逻辑
+
+# 查询函数，根据日期范围和地点/备注查询视频信息
+def query_videos(start_date, end_date, location_remark):
+    # 在这里执行查询操作，根据传入的参数查询相关视频信息
+    # 这里假设 get_videos_by_date_range是你自定义的查询函数
+    videos = db_option.get_videos_by_date_range(start_date, end_date)
+    print(videos)
+    return videos
+
+# 定义路由，处理查询请求和渲染查询结果页面
+@app.route('/query_results', methods=['GET'])
+def query_results():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    location_remark = request.args.get('location_remark')
+    print(start_date)
+    print(end_date)
+
+    if start_date and end_date:
+        # 执行查询操作，并获取查询结果
+        videos = query_videos(start_date, end_date, location_remark)
+        print(videos)
+        # 返回查询结果给前端页面
+        return render_template('result.html', videos=videos)
+    else:
+        return jsonify({'success': False, 'message': 'Missing query parameters'})
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
 
     # # 添加视频
-    # add_video('class1', 'description1', '12:34:56', '/path/to/video1.mp4')
-    # add_video('class2', 'description2', '18:34:56', '/path/to/video2.mp4')
+    # db_option.add_video('D:\Demo\\flask\\videos\\test1.mp4', 'class1', '2024-03-29', '坐标(1,1)', 'A区', 'description1')
+    # db_option.add_video('D:\Demo\\flask\\videos\\test2.mp4', 'class2', '2023-09-12', '坐标(10,300)', 'B区', 'description2')
     # # # 删除视频
     # # delete_video(1)
-    # video1 = get_video_by_id(2)
-    # video_all = get_all_videos()
-    # print(video1)
+    # # video1 = db_option.get_video_by_id(2)
+    # video_all = db_option.get_all_videos()
+    # # print(video1)
     # print(video_all)
     #
-    # # 修改视频信息
-    # update_video(2, 'new_class', 'new_description', '23:45:01', '/path/to/new_video.mp4')
-    # print(video_all)
+    # video2 = db_option.get_videos_by_time_range('2021-01-01', '2024-01-01')
+    # print(video2)
 
+    # # 修改视频信息
+    # db_option.update_video(2, 'new_class', 'new_description', '23:45:01', '/path/to/new_video.mp4')
+    # print(video_all)
+    #
     # # 添加用户
-    # add_user('user1', 'password1')
-    # add_user('user2', 'password2')
+    # db_option.add_user('user1', 'password1')
+    # db_option.add_user('user2', 'password2')
     #
     # # 查询用户
-    # user = get_user_by_id(1)
+    # user = db_option.get_user_by_id(1)
     # print(user)
     #
     # # 删除用户
-    # delete_user(1)
+    # db_option.delete_user(1)
     #
     # # 修改用户密码
-    # update_user_password(2, 'new_password')
-    # user2 = get_user_by_id(2)
+    # db_option.update_user_password(2, 'new_password')
+    # user2 = db_option.get_user_by_id(2)
     # print(user2)
---
+++ db_option.py
@@ -0,0 +1,165 @@
+# 四川大学 田亚君
+# 开发时间：2024-03-29 19:31
+# 导入Flask 模板
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+# 导入要用到的数据库 第三方库
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+# 连接数据库的配置
+# MySQL所在主机名
+HOSTNAME = "127.0.0.1"
+# MySQL监听的端口号，默认3306
+PORT = 3306
+# 连接MySQL的用户名，自己设置
+USERNAME = "root"
+# 连接MySQL的密码，自己设置
+PASSWORD = "123456"
+# MySQL上创建的数据库名称
+DATABASE = "detector"
+# 通过修改以下代码来操作不同的SQL比写原生SQL简单很多 --》通过ORM可以实现从底层更改使用的SQL
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
+db = SQLAlchemy(app)
+
+
+# 定义模型类user
+class User(db.Model):
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"<User user_id={self.user_id}, user_name={self.user_name}>"
+    # 表中的属性已经建好：user_id(key) user_name password
+    # id = db.Column(db.Integer, primary_key=True, comment="用户id")
+    # password = db.Column(db.String(20), nullable=False, comment="用户密码")
+
+
+# 定义模型类video
+class Video(db.Model):
+    __tablename__ = "videos"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    v_path = db.Column(db.String(255), nullable=False)
+    v_class = db.Column(db.String(255), nullable=True)
+    v_time = db.Column(db.Date, nullable=True)
+    v_location = db.Column(db.String(255), nullable=True)
+    v_region = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.String(20), nullable=True)
+
+    def __repr__(self):
+        return f"<Video id={self.id}, v_path={self.v_path}, v_class={self.v_class}, \
+        v_time={self.v_time}, v_location={self.v_location}, v_region={self.v_region}, description={self.description}>"
+
+
+# 查询用户
+def get_user_by_id(user_id):
+    with app.app_context():
+        return User.query.filter_by(user_id=user_id).first()
+
+
+# 添加用户
+def add_user(username, password):
+    with app.app_context():
+        new_user = User(user_name=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+# 删除用户
+def delete_user(user_id):
+    with app.app_context():
+        user = User.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+
+
+# 修改用户密码
+def update_user_password(user_id, new_password):
+    with app.app_context():
+        user = User.query.get(user_id)
+        if user:
+            user.password = new_password
+            db.session.commit()
+
+
+# 添加视频
+def add_video(v_path, v_class, v_time, v_location, v_region, description):
+    with app.app_context():
+        new_video = Video(v_path=v_path, v_class=v_class, v_time=v_time, v_location=v_location, v_region=v_region, description=description)
+        db.session.add(new_video)
+        db.session.commit()
+
+
+# 删除视频
+def delete_video(video_id):
+    with app.app_context():
+        video = Video.query.get(video_id)
+        if video:
+            db.session.delete(video)
+            db.session.commit()
+
+
+# 修改视频信息
+def update_video(video_id, new_v_path, new_v_class, new_v_time, new_v_location, new_v_region, new_description):
+    with app.app_context():
+        video = Video.query.get(video_id)
+        if video:
+            video.v_path = new_v_path
+            video.v_class = new_v_class
+            video.v_time = new_v_time
+            video.v_location = new_v_location
+            video.v_region = new_v_region
+            video.description = new_description
+            db.session.commit()
+
+# 根据视频存储时间查询视频信息
+def get_videos_by_date_range(start_time, end_time):
+    with app.app_context():
+        start_datetime = datetime.strptime(start_time, '%Y-%m-%d')
+        end_datetime = datetime.strptime(end_time, '%Y-%m-%d')
+        return Video.query.filter(Video.v_time.between(start_datetime, end_datetime)).all()
+
+# 根据视频ID查询视频信息
+def get_video_by_id(video_id):
+    with app.app_context():
+        return Video.query.get(video_id)
+
+
+# 根据视频分类查询视频信息
+def get_videos_by_class(v_class):
+    with app.app_context():
+        return Video.query.filter_by(v_class=v_class).all()
+
+
+# 查询所有视频信息
+def get_all_videos():
+    with app.app_context():
+        return Video.query.all()
+
+
+# 执行所有的创建表格的语句
+# 表已经建好了
+# with app.app_context():
+#     db.create_all()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
